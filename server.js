c397e2228bb3d4e997df32b0111c0eb7659e483a
Jaaystones <Joelpaul345@gmail.com>
2023-10-16 15:35:06 +0100

    :wrench:Server

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/', router)
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
+        res.sendFile(path.join(__dirname, '/views', '404.html'))
     } else if (req.accepts('json')) {
         res.json({ message: '404 Not Found' })
     } else {
