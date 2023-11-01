948926abfa5bf0b67c3af51f8bef8d36fba0eded
Jaaystones <Joelpaul345@gmail.com>
2023-11-01 16:42:37 +0100

    :wrench:Updated cors and frontend

--- frontend/my-notes/src/app/store.js
+++ frontend/my-notes/src/app/store.js
@@ -11,7 +11,7 @@ export const store = configureStore({
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: false
+    devTools: true
 })
 
 
--- frontend/my-notes/src/index.js
+++ frontend/my-notes/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import { disableReactDevtools } from '@fvilers/disable-react-devtools';
+//import { disableReactDevtools } from '@fvilers/disable-react-devtools';
 
-if (process.env.NODE_ENV !== 'production') disableReactDevtools();            
+//if (process.env.NODE_ENV !== 'production') disableReactDevtools();            
 
   
 const root = ReactDOM.createRoot(document.getElementById('root'));
